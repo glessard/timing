8e6a1474d08e8707755409db7914db1508bbc858
Guillaume Lessard <dhtnstff@gmail.com>
2017-08-08 09:52:06 -0600

    package description overhaul

--- Package.swift
+++ Package.swift
@@ -1,5 +1,29 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
+let name = "timing"
+
+#if swift(>=4.0)
+
 let package = Package(
-    name: "timing"
+  name: name,
+  products: [
+    .library(name: name, type: .static, targets: [name]),
+  ],
+  targets: [
+    .target(name: name),
+    .testTarget(
+      name: name+"Tests",
+      dependencies: [Target.Dependency(stringLiteral: name)]),
+  ],
+  swiftLanguageVersions: [3,4]
 )
+
+#else
+
+let package = Package(
+  name: name
+)
+
+#endif
