41f5f18c5a839c9e539c664a2308cf6a89319410
Guillaume Lessard <dhtnstff@gmail.com>
2019-02-14 18:10:36 -0700

    update package manifest

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -1,8 +1,6 @@
-// swift-tools-version:4.2
+// swift-tools-version:4.0
 
 import PackageDescription
-
-#if swift(>=4.0)
 
 let package = Package(
   name: "timing",
@@ -13,13 +11,5 @@ let package = Package(
     .target(name: "timing"),
     .testTarget(name: "timingTests", dependencies: ["timing"]),
   ],
-  swiftLanguageVersions: [.v3, .v4, .v4_2, .version("5")]
+  swiftLanguageVersions: [3,4]
 )
-
-#else
-
-let package = Package(
-  name: "timing"
-)
-
-#endif
